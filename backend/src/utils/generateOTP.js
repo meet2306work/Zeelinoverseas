c2ee0ac0cf2582322c11de89f0eb1cd351bcae9a
meet2306work <meet2306.work@gmail.com>
2026-08-03 23:17:44 +0530

    remove backend code

--- backend/src/utils/generateOTP.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const generateOTP = () => {
-  // Generate a cryptographically secure random 6-digit number
-  return crypto.randomInt(100000, 1000000).toString();
-};
-
-module.exports = generateOTP;
